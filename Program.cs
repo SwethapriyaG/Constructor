3e100f3a092142caaea7028600f7fe2b1081974e
Swethapriya G <107673962+SwethapriyaG@users.noreply.github.com>
2023-02-20 11:56:23 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Konstruktor
+{
+    class Program
+    {
+        static string vNamn;
+        static float vKilo, vPris;
+        static void Main(string[] args)
+        {
+            Fruit apple = new Fruit("apple", "geen", 30, 29, true, false);
+
+            Console.WriteLine("Välkommen till frukt fariken!");
+            Console.WriteLine(apple.FruitText());
+
+            Fruit obj1 = new Fruit("Banana", "yello", 12, 10, true, false);
+            Console.WriteLine("Välkommen till frukt fariken!");
+            Console.WriteLine(obj1.FruitText());
+
+            VegInfo();
+            Food veg = new Food(vNamn, vKilo, vPris);
+            Console.WriteLine(veg.VegText());
+            
+        }
+        static void VegInfo()
+        {
+            
+
+            Console.WriteLine("Grönsaker Namn?");
+            vNamn = Console.ReadLine();
+            Console.WriteLine("Grönsaker Vikt i KG?");
+            string vikt = Console.ReadLine();
+            
+           float.TryParse(vikt, out vKilo);
+
+           Console.WriteLine("Grönsaker pris i SEK?");
+            string prise = Console.ReadLine();
+            float.TryParse(prise, out vPris);
+            
+
+
+
+
+
+        }
+    }
+}
