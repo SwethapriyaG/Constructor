cb36e6b79c26837ad135341d2d547e0bbcc04544
Swethapriya G <107673962+SwethapriyaG@users.noreply.github.com>
2023-02-20 11:54:52 +0100

    Add files via upload

---
+++ Fruit.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Konstruktor
+{
+    class Fruit
+    {
+        string namn, farg;
+        float kcal, pris;
+        bool atbar, skal;
+        public Fruit(string fruktNamn, string fruktFarg, float fruktKcal, float fruktPris, bool fruktAtbar, bool fruktSkal)
+        {
+            namn = fruktNamn;
+            farg = fruktFarg;
+            kcal = fruktKcal;
+            pris = fruktPris;
+            atbar = fruktAtbar;
+            skal = fruktSkal;
+
+        }
+        private string FruitInfo()
+        {
+            string fruitInfo;
+            fruitInfo = "Fruktnamn: " + namn + "\nFruktfarg: " + farg;
+            return fruitInfo;
+        }
+
+        public string FruitText()
+        {
+            string publicFruitInfo;
+            publicFruitInfo = FruitInfo();
+            return publicFruitInfo;
+
+        }
+    }
+}
